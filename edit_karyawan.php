a89d95687c47da837c2287cdcc2b365a9ba58f08
muhammadzidane <muhammmadzidanealsaadawi@gmail.com>
2020-11-12 22:56:13 +0700

    Modul edit_karyawan.php/edit_barang.php - selesai

--- edit_karyawan.php
+++ edit_karyawan.php
@@ -3,9 +3,6 @@ session_start();
 require_once 'files_backend_ajax/php_functions.php';
 cek_session();
 
-$conn 			= mysqli_connect("localhost","root","","tugas_inventaris");
-
-
 // Mengambil value dari database untuk mengisi tag <input> value 
 $kode_karyawan 	= (isset($_GET['kode_karyawan'])) ? $_GET['kode_karyawan'] : "";
 $nama_karyawan 	= (isset($_GET['nama_karyawan'])) ? $_GET['nama_karyawan'] : "";
@@ -90,71 +87,7 @@ while ($data = mysqli_fetch_assoc($query)) {
 			<a href="daftar_pegawai.php"><button id='buttonBatalEdit' class='btn btn-danger'>Batal</button></a>
 		</div>
 	</div>
-	<script>
-		$(document).ready(function() {
-
-			$("#posisi_jabatan").val("<?= $posisi_jabatan; ?>"); // Nilai pertama posisi jabatan saat load event berjalan 
-
-			$("#acceptEdit").click(function(e) {
-				let confirmEdit = confirm("Apakah Anda Yakin Ingin Menambahkan Data Barang?");
-				if (confirmEdit) {
-					let valKodeKaryawan 			= $("#kode_karyawan").val();
-					let valNamaKaryawan 			= $("#nama_karyawan").val();
-					let valPosisiJabatan 		= $("#posisi_jabatan").val();
-					let valEmail 					= $("#email").val();
-					let valPendidikanTerakhir	= $("#pendidikan_terakhir").val();
-					let valAlamat 					= $("#alamat").val();
-					let valFoto 					= $("#foto").val();
-					validasiKosong(valKodeKaryawan,"#kode_karyawan");
-					validasiNomer(valKodeKaryawan, "#kode_karyawan", 8)
-					validasiKosong(valNamaKaryawan, "#nama_karyawan");
-					validasiKosong(valEmail, "#email");
-					validasiEmailEventClick(valEmail, "#email");
-					validasiKosong(valPendidikanTerakhir, "#pendidikan_terakhir");
-					validasiKosong(valAlamat, "#alamat");
-					validasiKosong(valFoto, "#foto");
-
-					if ($(".pesanValidasi").text() == "") {
-						
-						<?php 
-						$_SESSION['kode_karyawan'] = $kode_karyawan;
-						?>	
-
-						$.ajax({
-							url 	: "files_backend_ajax/backend_daftar_karyawan.php",
-							type 	: "POST",
-							data 	: { validasiDuplikatKey : valKodeKaryawan },
-							success : function(responseText, success) {
-								if (responseText == "berhasil") {
-									$.post('files_backend_ajax/backend_daftar_karyawan.php',{
-										acceptEdit 				: true, 
-										urlKodeKaryawan 		: "<?php echo $kode_karyawan; ?>",
-										urlNamaKaryawan 		: "<?php echo $nama_karyawan; ?>",
-										valKodeKaryawan 		: valKodeKaryawan,
-										valNamaKaryawan 		: valNamaKaryawan,
-										valPosisiJabatan 		: valPosisiJabatan,
-										valEmail 				: valEmail,
-										valPendidikanTerakhir: valPendidikanTerakhir,
-										valAlamat 				: valAlamat,
-										valFoto 					: valFoto }, 
-										function(responseText) {
-											location.assign(`daftar_pegawai.php?berhasil_edit=${ responseText }`);
-										}
-										);
-								}
-								else {
-									$("#kode_karyawan").next().html(`<small>${ responseText }</small>`).addClass('pesanValidasi');
-									e.preventDefault();
-								}
-							}					
-						});
-					}
-				}
-		// 	});
-
-	});
-		});
-	</script>
-<script src="jquery_functions/js_functions.js"></script>
+	<script src=src_moduls/edit_karyawan.js></script>
+	<script src="src_moduls/js_functions.js"></script>
 </body>
 </html>
---
+++ src_moduls/edit_karyawan.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+	"use strict";
+
+	$("#posisi_jabatan").val("<?= $posisi_jabatan; ?>"); // Nilai pertama posisi jabatan saat load event berjalan 
+	$("#acceptEdit").click(function(e) {
+		let confirmEdit = confirm("Apakah Anda Yakin Ingin Menambahkan Data Barang?");
+		if (confirmEdit) {
+			let valKodeKaryawan 			= $("#kode_karyawan").val();
+			let valNamaKaryawan 			= $("#nama_karyawan").val();
+			let valPosisiJabatan 		= $("#posisi_jabatan").val();
+			let valEmail 					= $("#email").val();
+			let valPendidikanTerakhir	= $("#pendidikan_terakhir").val();
+			let valAlamat 					= $("#alamat").val();
+			let valFoto 					= $("#foto").val();
+			
+			validasiKosong(valKodeKaryawan,"#kode_karyawan");
+			validasiNomer(valKodeKaryawan, "#kode_karyawan", 8)
+			validasiKosong(valNamaKaryawan, "#nama_karyawan");
+			validasiKosong(valEmail, "#email");
+			validasiEmailEventClick(valEmail, "#email");
+			validasiKosong(valPendidikanTerakhir, "#pendidikan_terakhir");
+			validasiKosong(valAlamat, "#alamat");
+			validasiKosong(valFoto, "#foto");
+			if ($(".pesanValidasi").text() == "") {
+				$.ajax({
+					url 	: "files_backend_ajax/backend_daftar_karyawan.php",
+					type 	: "POST",
+					data 	: { validasiDuplikatKey : valKodeKaryawan },
+					success : function(responseText, success) {
+						if (responseText == "berhasil") {
+							$.post('files_backend_ajax/backend_daftar_karyawan.php',{
+								acceptEdit 				: true, 
+								urlKodeKaryawan 		: "<?php echo $kode_karyawan; ?>",
+								urlNamaKaryawan 		: "<?php echo $nama_karyawan; ?>",
+								valKodeKaryawan 		: valKodeKaryawan,
+								valNamaKaryawan 		: valNamaKaryawan,
+								valPosisiJabatan 		: valPosisiJabatan,
+								valEmail 				: valEmail,
+								valPendidikanTerakhir: valPendidikanTerakhir,
+								valAlamat 				: valAlamat,
+								valFoto 					: valFoto }, 
+								function(responseText) {
+									location.assign(`daftar_pegawai.php?berhasil_edit=${ responseText }`);
+								}
+								);
+						}
+						else {
+							$("#kode_karyawan").next().html(`<small>${ responseText }</small>`).addClass('pesanValidasi');
+							e.preventDefault();
+						}
+					}					
+				});
+			}
+		}
+	});
+});
